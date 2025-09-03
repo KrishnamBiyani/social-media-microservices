1d9268a42284c414dc0d3b1e1fcfc1597cca43fa
KrishnamBiyani <krishnambiyani5@gmail.com>
2025-09-03 23:17:08 +0530

    feat:add errorhandler add logger

---
+++ identity-service/src/midllewares/errorHandler.js
@@ -0,0 +1,11 @@
+const logger = reqire("../utils/logger");
+
+const errorHandler = (err, req, res, next) => {
+  logger.error(err.stack);
+
+  res.status(err.status || 500).json({
+    message: err.message || "Internal server error",
+  });
+};
+
+module.exports = errorHandler;
