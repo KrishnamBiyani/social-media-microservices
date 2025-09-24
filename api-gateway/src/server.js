7c76f8cfedbac9a6a8c51fd9ee611e68c2a59262
KrishnamBiyani <krishnambiyani5@gmail.com>
2025-09-24 23:38:33 +0530

    WIP: working on API Gateway integration

---
+++ api-gateway/src/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const Redis = require("ioredis");
+const helmet = require("helmet");
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis");
+const logger = require("./utils/logger");
+const proxy = require("express-http-proxy");
+const errorHandler = require("./middleware/errorhandler");
+const { validateToken } = require("./middleware/authMiddleware");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+//rate limiting
+const ratelimitOptions = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({ success: false, message: "Too many requests" });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
